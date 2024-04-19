f1e58d953b8859a771fac97a803350bc13f355a6
chj <2639075023@qq.com>
2024-04-19 16:36:40 +0800

    update

--- test/text/测试.c
+++ test/text/测试.c
@@ -6,27 +6,11 @@
 
 int main()
 {
-    int n = 0 ;
-    scanf("%d",&n);
-    switch (n)
+    int count = 10; //循环条件初始化
+    while (count > 10) //循环条件判定
     {
-        int a = 1;
-        case 1:
-        printf("%d\n",a);
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-            puts("周内");
-            break;
-        case 6:
-            puts("周六");
-            break;
-        case 7:
-            puts("周日");
-            break;
-        default:
-            break;
+        printf("%d\n", count);
+        count--; //循环条件更新
     }
     return 0;
 }

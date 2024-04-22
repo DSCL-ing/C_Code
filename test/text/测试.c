6d17f099131d9aa5d8b188a9ed9fd85db0ae868b
chj <2639075023@qq.com>
2024-04-22 13:55:38 +0800

    struct 柔性数组 union

--- test/text/测试.c
+++ test/text/测试.c
@@ -1,9 +1,26 @@
 ﻿#include<stdio.h>
 
+struct data
+{
+    int num;
+    int arr[0];
+};
+
+void fun()
+{
+
+    int a = 10;
+    int b = 20;
+    int arr[5] = {1,2,3,4,5};
+    printf("%p\n",a);
+    printf("%p\n",b);
+    printf("%p\n",&arr);
+    printf("%p\n",arr[1]);
+    printf("%p\n",arr[2]);
+}
 
 int main()
 {
-    const int a[] = {1,2,3,4,5};
-    int b = 20;
+fun();
     return 0;
 }

41d6abad9bfda215ab89a0557e54959716264ab8
chj <2639075023@qq.com>
2024-04-22 18:41:57 +0800

    union

--- test/text/测试.c
+++ test/text/测试.c
@@ -1,26 +1,22 @@
 ﻿#include<stdio.h>
 
-struct data
-{
-    int num;
-    int arr[0];
-};
-
 void fun()
 {
-
-    int a = 10;
-    int b = 20;
-    int arr[5] = {1,2,3,4,5};
-    printf("%p\n",a);
-    printf("%p\n",b);
-    printf("%p\n",&arr);
-    printf("%p\n",arr[1]);
-    printf("%p\n",arr[2]);
 }
+
+union un
+{
+    int i;
+    char a[4];
+};
 
 int main()
 {
-fun();
+    union un x;
+    x.a[0] = 0x78;
+    x.a[1] = 0x56;
+    x.a[2] = 0x34;
+    x.a[3] = 0x12;
+    printf("0x%x\n",x.i); //vs:0x12345678
     return 0;
 }

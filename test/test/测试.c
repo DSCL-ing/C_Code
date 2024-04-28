6b9e9a17016338c63ab5ae79e3217009d5355d69
chj <2639075023@qq.com>
2024-04-28 22:45:55 +0800

    update

--- test/test/测试.c
+++ test/test/测试.c
@@ -1,11 +1,20 @@
 ﻿#include<stdio.h>
 
-
-
 int main()
 {
+#if 0
     int a = 10;
-    int* p = &a;
-    //int b = 20/*p;
+    int b = 20;
+
+    char c = 'abc';
+    printf("%c",c);
+#endif
+
+    printf("%d\n",sizeof(1));  //4
+    printf("%d\n",sizeof("1"));//2
+    printf("%d\n",sizeof('1'));//4
+    char c = '1';
+    printf("%d\n",sizeof(c));  //1
+
     return 0;
 }

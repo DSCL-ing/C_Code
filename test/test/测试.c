e383ab173f428147203b35ed78e362b01f0ba1f3
chj <2639075023@qq.com>
2024-04-29 16:37:27 +0800

    单引号

--- test/test/测试.c
+++ test/test/测试.c
@@ -2,19 +2,21 @@
 
 int main()
 {
-#if 0
+#if 1
     int a = 10;
     int b = 20;
 
-    char c = 'abc';
+    char c = 'abcd';
     printf("%c",c);
 #endif
 
+#if 0
     printf("%d\n",sizeof(1));  //4
     printf("%d\n",sizeof("1"));//2
-    printf("%d\n",sizeof('1'));//4
-    char c = '1';
+    printf("%d\n",sizeof('abc'));//4
+    char c = 'abcd';
     printf("%d\n",sizeof(c));  //1
+#endif
 
     return 0;
 }

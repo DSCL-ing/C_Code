3f25bbbf4d883cb588baf6fb564bba52e1dbd35e
chj <2639075023@qq.com>
2024-04-30 18:26:54 +0800

    异或

--- test/test/测试.c
+++ test/test/测试.c
@@ -1,32 +1,13 @@
 ﻿#include<stdio.h>
 
 
-int show()
-{
-    printf("aaaaaaa");
-    return 1;
-}
-
 int main()
 {
-#if 1
-   int flag = 0;
-   scanf("%d",&flag);
-   flag && show();  //使用短路实现的if语句效果
-   //如果表达式左边为假,则表达式右边不再执行,这样的语句就称为短路.
-
-   scanf("%d",&flag);
-   flag || show();  //使用短路实现的if not语句
-
-#endif
-
-#if 0
-    printf("%d\n",sizeof(1));  //4
-    printf("%d\n",sizeof("1"));//2
-    printf("%d\n",sizeof('abc'));//4
-    char c = 'abcd';
-    printf("%d\n",sizeof(c));  //1
-#endif
-
+    int a = 10;
+    int b = 20;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("a = %d, b = %d\n",a,b);
     return 0;
 }

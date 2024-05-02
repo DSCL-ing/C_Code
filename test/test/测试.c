02ae7f124a096dc8c81a25cf401a33d13bb21649
chj <2639075023@qq.com>
2024-05-02 22:50:15 +0800

    四种取整方式

--- test/test/测试.c
+++ test/test/测试.c
@@ -3,12 +3,14 @@
 
 int main()
 {
-    int a = -2.9;
-    int b = 12.9;
-    printf("%d\n",a);
-    printf("%d\n",b);
-    printf("%f\n",trunc(b));
-    printf("%d\n",trunc(b)); 
-    printf("%d\n",(int)trunc(b));
+    const char * format = "%.1f \t%.1f \t%.1f \t%.1f \t%.1f\n"; 
+    printf("value\tround\tfloor\tceil\ttrunc\n");
+    printf("-----\t-----\t-----\t----\t-----\n");
+    printf(format, 2.3, round(2.3), floor(2.3), ceil(2.3), trunc(2.3));
+    printf(format, 3.8, round(3.8), floor(3.8), ceil(3.8), trunc(3.8));
+    printf(format, 5.5, round(5.5), floor(5.5), ceil(5.5), trunc(5.5));
+    printf(format, -2.3, round(-2.3), floor(-2.3), ceil(-2.3), trunc(-2.3));
+    printf(format, -3.8, round(-3.8), floor(-3.8), ceil(-3.8), trunc(-3.8));
+    printf(format, -5.5, round(-5.5), floor(-5.5), ceil(-5.5), trunc(-5.5));
     return 0;
 }

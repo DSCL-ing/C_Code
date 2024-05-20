953c2734f8697d85471a5c63b5d90d5aa8fa9f04
chj <2639075023@qq.com>
2024-05-20 09:32:31 +0800

    可变参数

--- test/test/测试.c
+++ test/test/测试.c
@@ -1,25 +1,39 @@
 ﻿#include <stdio.h>
-#include<stdarg.h>
+#include <stdarg.h>
 
 FindMax(int num, ...)
 {
     va_list arg;
-    va_start(arg,num);
+    va_start(arg, num);
     int max = 0;
-    max = va_arg(arg,int);
-    for()
+    max = va_arg(arg, int);
+    for (int i = 0; i < num - 1; i++)
+    {
+        int cur = va_arg(arg, int);
+        if (cur > max)
+        {
+            max = cur;
+        }
+    }
+    va_end(arg);
 
-    return;
+    return max;
 }
 
 int main()
 {
 #if defined(_INC_STDARG)
-    #pragma message("included stdarg.h");
+#pragma message("included stdarg.h")
 #else
-    #pragma message("no included stdarg.h");
+#pragma message("no included stdarg.h");
 #endif
 
-     
+    char a = 'a';
+    char b = 'b';
+    char c = 'c';
+    char d = 'd';
+    char e = 'e';
+    int max = FindMax(5, a, b, c, d, e);
+    printf("max = %d\n", max);
     return 0;
 }

c387a8c0c72702f3acbd2ecc33dccc93e2a000c9
chj <2639075023@qq.com>
2024-05-22 17:41:26 +0800

    update

--- test/test/测试.c
+++ test/test/测试.c
@@ -1,12 +1,25 @@
-﻿#include <stdio.h>
-#include <stdarg.h>
+﻿#include<stdio.h>
+#include<stdarg.h>
 
+/*
+micro:
+    __FILE__ :  当前代码所在文件
+    __LINE__ :  第几行
+   */
 
-int main(int argc, char* argv[], char* envp[])
+   //2. 
+#define LOG(fmt,...) printf("[%s:%d]" fmt,__FILE__,__LINE__,__VA_ARGS__);
+/*
+   1.fmt是个字符串,字符串和字符串能够拼接
+ */
+
+int main()
 {
-    for (int i = 0; envp[i]; i++)
-    {
-        printf("%s\n",envp[i]);
-    }
+    //1.引入
+    //printf("[%s:%d] %s %d\n",__FILE__,__LINE__,"测试",999) ;
+    LOG("hello");
+
+
     return 0;
+
 }

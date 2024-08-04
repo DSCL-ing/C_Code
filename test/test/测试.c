df08fae59b7ff230bb75fb85536b3d5127de35d7
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-08-04 22:49:44 +0800

    update

--- test/test/测试.c
+++ test/test/测试.c
@@ -1,25 +1,13 @@
-﻿#include<stdio.h>
-#include<stdarg.h>
+﻿
+//#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
 
-/*
-micro:
-    __FILE__ :  当前代码所在文件
-    __LINE__ :  第几行
-   */
-
-   //2. 
-#define LOG(fmt,...) printf("[%s:%d]" fmt,__FILE__,__LINE__,__VA_ARGS__);
-/*
-   1.fmt是个字符串,字符串和字符串能够拼接
- */
-
-int main()
-{
-    //1.引入
-    //printf("[%s:%d] %s %d\n",__FILE__,__LINE__,"测试",999) ;
-    LOG("hello");
-
-
+int main() {
+    const char* a = "hello";
+    const char* b = "hello";
+    strstr();
+    printf("%d",strcmp(a,b));
     return 0;
-
 }
